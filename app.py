234f31f7fd974ec2daff9e2fb3eb246abfb78f8f
andrewbigelow <abigelow@ucsd.edu>
2026-02-11 11:38:41 -0800

    changed running port for Render use

--- app.py
+++ app.py
@@ -371,5 +371,6 @@ def next_round():
     game_state['current_round'] += 1
     return jsonify({'round': game_state['current_round']})
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
